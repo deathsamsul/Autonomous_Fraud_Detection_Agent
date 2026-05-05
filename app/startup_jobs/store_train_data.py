6050fa0482c77bc982953904e1bf60a1ad8065e4
Samsul <deathsamsul@gmail.com>
2026-05-05 05:41:41 +0000

    upload function permission error

--- app/startup_jobs/store_train_data.py
+++ app/startup_jobs/store_train_data.py
@@ -10,27 +10,6 @@ TRAINING_DATA_STORE_URL = os.environ.get(
 DATA_DIR = Path(os.environ.get("DATA_DIR", "/opt/datasets"))
 
 
-# def upload_training_data_to_db(data_path: str) -> None:
-
-#     engine = create_engine(TRAINING_DATA_STORE_URL)
-#     df = pd.read_csv(data_path)
-
-#     raw_columns = [
-#         "merchant", "category", "amt", "gender", "city", "state", "zip",
-#         "lat", "long", "city_pop", "job", "unix_time", "merch_lat",
-#         "merch_long", "trans_date_trans_time", "dob", "is_fraud"]
-
-#     df = df[raw_columns]
-#     df.rename(columns={"is_fraud": "actual_label"}, inplace=True)
-
-#     df.to_sql(
-#         "training_data",
-#         engine,
-#         if_exists="replace",
-#         index=False)
-
-#     print(f"Uploaded {len(df)} rows to training_data table")
-
 
 def upload_training_data_to_db(data_path: str) -> None:
     engine = create_engine(TRAINING_DATA_STORE_URL)
