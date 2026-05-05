d8be735c0cf309a8cf2b97df54df8c22c9023093
Samsul <deathsamsul@gmail.com>
2026-05-05 05:39:29 +0000

    fix startup image in upload training job

--- app/startup_jobs/store_train_data.py
+++ app/startup_jobs/store_train_data.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 TRAINING_DATA_STORE_URL = os.environ.get(
     "TRAINING_DATA_STORE_URL",
@@ -10,26 +10,58 @@ TRAINING_DATA_STORE_URL = os.environ.get(
 DATA_DIR = Path(os.environ.get("DATA_DIR", "/opt/datasets"))
 
 
-def upload_training_data_to_db(data_path: str) -> None:
+# def upload_training_data_to_db(data_path: str) -> None:
 
+#     engine = create_engine(TRAINING_DATA_STORE_URL)
+#     df = pd.read_csv(data_path)
+
+#     raw_columns = [
+#         "merchant", "category", "amt", "gender", "city", "state", "zip",
+#         "lat", "long", "city_pop", "job", "unix_time", "merch_lat",
+#         "merch_long", "trans_date_trans_time", "dob", "is_fraud"]
+
+#     df = df[raw_columns]
+#     df.rename(columns={"is_fraud": "actual_label"}, inplace=True)
+
+#     df.to_sql(
+#         "training_data",
+#         engine,
+#         if_exists="replace",
+#         index=False)
+
+#     print(f"Uploaded {len(df)} rows to training_data table")
+
+
+def upload_training_data_to_db(data_path: str) -> None:
     engine = create_engine(TRAINING_DATA_STORE_URL)
+
     df = pd.read_csv(data_path)
 
     raw_columns = [
         "merchant", "category", "amt", "gender", "city", "state", "zip",
         "lat", "long", "city_pop", "job", "unix_time", "merch_lat",
-        "merch_long", "trans_date_trans_time", "dob", "is_fraud"]
+        "merch_long", "trans_date_trans_time", "dob", "is_fraud"
+    ]
 
     df = df[raw_columns]
     df.rename(columns={"is_fraud": "actual_label"}, inplace=True)
 
-    df.to_sql(
-        "training_data",
-        engine,
-        if_exists="replace",
-        index=False)
+    df["trans_date_trans_time"] = pd.to_datetime(df["trans_date_trans_time"])
+    df["dob"] = pd.to_datetime(df["dob"]).dt.date
+    df["zip"] = df["zip"].astype(str)
 
-    print(f"Uploaded {len(df)} rows to training_data table")
+    with engine.begin() as conn:
+        conn.execute(text("TRUNCATE TABLE public.fraud_training_data"))
+
+    df.to_sql(
+        "fraud_training_data",
+        engine,
+        schema="public",
+        if_exists="append",
+        index=False
+    )
+
+    print(f"Uploaded {len(df)} rows to public.fraud_training_data")
 
 
 if __name__ == "__main__":
@@ -46,3 +78,6 @@ if __name__ == "__main__":
 #   -v $(pwd)/datasets:/opt/datasets \
 #   -e TRAINING_DATA_STORE_URL="postgresql+psycopg2://training_user:train123@postgres-cluster-rw.database.svc.cluster.local:5432/appdata" \
 #   image
+
+#   python app/startup_jobs/store_train_data.py
+#  python3 -m app.startup_jobs.store_train_data
